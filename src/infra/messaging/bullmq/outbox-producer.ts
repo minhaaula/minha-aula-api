5af60e75f08c782cadee50cd5c0fcd864b9e09b9
Cassiano Souza <redhotcassiano@gmail.com>
2025-09-24 19:06:08 -0300

    chore: atualizando redis

--- src/infra/messaging/bullmq/outbox-producer.ts
+++ src/infra/messaging/bullmq/outbox-producer.ts
@@ -3,7 +3,12 @@ import { OutboxRepository } from '../../../ports/repositories/outbox.repo';
 
 export class OutboxProducer implements OutboxRepository {
     private queue = new Queue('outbox', {
-        connection: { host: process.env.REDIS_HOST, port: +(process.env.REDIS_PORT ?? 6379) }
+        connection: {
+            host: process.env.REDIS_HOST,
+            port: +(process.env.REDIS_PORT ?? 6379),
+            ...(process.env.REDIS_USER ? { username: process.env.REDIS_USER } : {}),
+            ...(process.env.REDIS_PASSWORD ? { password: process.env.REDIS_PASSWORD } : {}),
+        }
     });
     async enqueue(event: { type: string; payload: unknown; aggregateId: string }): Promise<void> {
         await this.queue.add(event.type, event, { removeOnComplete: true, attempts: 5, backoff: { type: 'exponential', delay: 1500 } });
--- src/infra/messaging/bullmq/outbox-worker.ts
+++ src/infra/messaging/bullmq/outbox-worker.ts
@@ -1,5 +1,12 @@
 import { Worker } from 'bullmq';
 
+const connection = {
+    host: process.env.REDIS_HOST,
+    port: +(process.env.REDIS_PORT ?? 6379),
+    ...(process.env.REDIS_USER ? { username: process.env.REDIS_USER } : {}),
+    ...(process.env.REDIS_PASSWORD ? { password: process.env.REDIS_PASSWORD } : {}),
+};
+
 new Worker('outbox', async job => {
     console.log('[OUTBOX]', job.name, job.data);
-}, { connection: { host: process.env.REDIS_HOST, port: +(process.env.REDIS_PORT ?? 6379) } });
+}, { connection });
