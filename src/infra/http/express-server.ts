46eed52c512579d973a4b416edfb139cebd64efb
Cassiano Souza <redhotcassiano@gmail.com>
2025-09-30 21:20:31 -0300

    add config cors

--- src/infra/http/express-server.ts
+++ src/infra/http/express-server.ts
@@ -85,6 +85,15 @@ interface AppDependencies {
 export function makeServer(deps: AppDependencies & Record<string, any>) {
     const app = express();
     app.use(express.json());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(204);
+        }
+        next();
+    });
     app.use(requestLogger);
 
     const mount = (path: string, router: Router, options?: MountOptions) => {
