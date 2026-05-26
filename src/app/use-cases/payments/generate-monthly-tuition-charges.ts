9f7c98f09cd96b34a15b7ff42ab36c1345fb0162
Cassiano De Souza E Silva <redhotcassiano@gmail.com>
2026-05-26 10:16:18 -0300

    refactor: merge main

--- src/app/use-cases/payments/generate-monthly-tuition-charges.ts
+++ src/app/use-cases/payments/generate-monthly-tuition-charges.ts
@@ -19,7 +19,6 @@ import {
   resolveNextTuitionDueDate,
   shouldGenerateTuitionChargeInWindow,
 } from "./resolve-next-tuition-due-date";
-import { coerceToDate } from "../../../shared/date-utils";
 
 type GenerateMonthlyTuitionChargesInput = {
   targetMonth?: number; // 1-12, se não fornecido usa o próximo mês
